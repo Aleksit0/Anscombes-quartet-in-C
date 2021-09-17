506a3a979401b65333f7d78520e9c8be3c74374c
Aleksit0 <50078646+Aleksit0@users.noreply.github.com>
2021-09-17 15:07:43 +0200

    added template

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 
 # INIT
-app = Flask(__name__)
+app = Flask(__name__, template_folder = 'template')
+
+# TODO: PREVESTI OPISE VREMENA NA SRPSKI
 
 @app.route('/')
 
 def idnex():
     API_KEY = 'http://api.openweathermap.org/data/2.5/weather?q={}&APPID=4160714ccefddd8b61c83a198f86a47b'
-    city_name = 'Moscow'
+    city_name = 'Banjaluka'
 
-    # PRINT JSON OF THAT CITY DATA IN CONSOLE
+    #! PRINT JSON OF THAT CITY DATA IN CONSOLE
     r = requests.get(API_KEY.format(city_name)).json()
     print(r, '\n')
 
-    # PUT NEEDED DATA IN PYTHON DICTIONARY
+    #! PUT NEEDED DATA IN PYTHON DICTIONARY
     weather_dict = {
         'city': city_name,
         'temperature': round(r['main']['temp'] - 273.15),
@@ -23,8 +25,9 @@ def idnex():
     }
     print(weather_dict)
 
-    return '<h1>Ovo je weather app</h1>'
+    #return weather_dict
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    # WHEN PRODUCTION, debug = False
+    #! WHEN PRODUCTION, debug = False
     app.run(debug = True)  
