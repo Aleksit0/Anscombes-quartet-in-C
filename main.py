31a653d8a4c32470ed51471a0a4dabf0819e325c
Aleksit0 <50078646+Aleksit0@users.noreply.github.com>
2021-09-17 14:17:37 +0200

    fix

--- main.py
+++ main.py
@@ -4,29 +4,15 @@ import requests
 # INIT
 app = Flask(__name__)
 
-@app.route('/city')
+@app.route('/')
 
-def search_city():
-    API_KEY = 'api.openweathermap.org/data/2.5/weather?q={city name}&appid4160714ccefddd8b61c83a198f86a47b'
-    city_name = requests.args.get('q') # CITY NAME AS ARGUMENT
+def idnex():
+    API_KEY = 'http://api.openweathermap.org/data/2.5/weather?q={}&APPID=4160714ccefddd8b61c83a198f86a47b'
+    city_name = 'Moscow'
     
-    # CONVERT RESPONSE TO PY DICTIONARY
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&APPID={API_KEY}'
-    response = requests.get(url).json()
-    
-    if response.get('cod') != 200:
-        return f'Error getting data for {city_name.title()}. Invalid city name.'
-        
-    # CONVERT TO CELSIUS
-    temp = response.get('main', {}).get('temp')
-    if temp:
-        temp_to_celsius = round(temp - 273.15, 2)    
-        return f'The current temp of {city_name.title()} is {temp_to_celsius} &#8451'
-    else:
-        return f'Error getting data for {city_name.title()}, check your city name.'
-    
-    
-def index():
+    # PRINT JSON OF THAT CITY DATA IN CONSOLE
+    r = requests.get(API_KEY.format(city_name)).json()
+    print(r)
     return '<h1>Ovo je weather app</h1>'
 
 if __name__ == '__main__':
