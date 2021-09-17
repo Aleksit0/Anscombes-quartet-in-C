9d92a35f9cdd98cc475bbf9f7c8a77c1455c936a
Aleksit0 <50078646+Aleksit0@users.noreply.github.com>
2021-09-17 14:42:16 +0200

    convert current temp to celsius

--- main.py
+++ main.py
@@ -9,10 +9,20 @@ app = Flask(__name__)
 def idnex():
     API_KEY = 'http://api.openweathermap.org/data/2.5/weather?q={}&APPID=4160714ccefddd8b61c83a198f86a47b'
     city_name = 'Moscow'
-    
+
     # PRINT JSON OF THAT CITY DATA IN CONSOLE
     r = requests.get(API_KEY.format(city_name)).json()
-    print(r)
+    print(r, '\n')
+
+    # PUT NEEDED DATA IN PYTHON DICTIONARY
+    weather_dict = {
+        'city': city_name,
+        'temperature': round(r['main']['temp'] - 273.15),
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon']
+    }
+    print(weather_dict)
+
     return '<h1>Ovo je weather app</h1>'
 
 if __name__ == '__main__':
