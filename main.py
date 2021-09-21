53a08785c3b249360abeda9eb12c75bcd748dd54
Aleksit0 <aleksapython@gmail.com>
2021-09-21 21:59:53 +0200

    added better weather info

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import requests
-
 
 # IF CSS WONT UPDATE IN CHROME BROWSER HOLD SHIFT AND CLICK RELOAD TO DELETE CACHE
 
@@ -17,6 +16,7 @@ def idnex():
     city_name = 'Banjaluka'
 
     #! PRINT JSON OF THAT CITY DATA IN CONSOLE
+    #r = requests.get(API_KEY).json()
     r = requests.get(API_KEY.format(city_name)).json()
     print(r, '\n')
 
@@ -37,10 +37,14 @@ def idnex():
         else:
             return 'Obucite se komotnije, vrijeme je toplije.'
 
-    print(weather_dict)
+    #! FORMAT JSON
+    print(weather_dict, "\n")
+    
+    #! LOOP THROUGH WEATHER INFO
+    better_info = [weather_dict['city'], weather_dict['temperature'], weather_dict['description']]
 
     #return weather_dict
-    return render_template('index.html', content = weather_dict, odjeca = obuci_se())
+    return render_template('index.html', city = better_info[0], temp = better_info[1], description = better_info[2], odjeca = obuci_se())
 
 if __name__ == '__main__':
     #! WHEN PRODUCTION, debug = False
